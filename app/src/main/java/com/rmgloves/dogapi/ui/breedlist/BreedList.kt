ab26302aef7a3e7d39581e78b3b5b4443111eaa5
Robert Glover <robertglover@Roberts-MacBook-Pro.local>
2025-03-31 18:07:37 -0400

    Added breed filter

--- app/src/main/java/com/rmgloves/dogapi/ui/breedlist/BreedList.kt
+++ app/src/main/java/com/rmgloves/dogapi/ui/breedlist/BreedList.kt
@@ -2,22 +2,32 @@ package com.rmgloves.dogapi.ui.breedlist
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.rmgloves.dogapi.data.model.Breed
-import kotlinx.serialization.json.Json
+import com.rmgloves.dogapi.R
 import timber.log.Timber
 
 @Composable
@@ -43,22 +53,62 @@ fun BreedList(
         }
 
         is BreedListState.Success -> {
-            LazyColumn(
+            var query by remember { mutableStateOf("") }
+            val filteredBreeds = immutableState.breedList.filter {
+                val q = query.trim().lowercase()
+                it.breed.contains(q, ignoreCase = true) || it.subBreed?.contains(q, ignoreCase = true) == true
+            }
+            Column(
                 modifier = Modifier.fillMaxSize()
             ) {
-                items(immutableState.breedList) { breed ->
-                    Text(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .clickable {
-                                goToBreed(breed.encodeClass())
-                            }
-                            .padding(horizontal = 16.dp, vertical = 4.dp),
-                        text = breed.getDisplayString(),
-                        style = typography.bodyLarge
-                    )
+                SearchField(query) {
+                    query = it
+                }
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    items(filteredBreeds) { breed ->
+                        Text(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .clickable {
+                                    goToBreed(breed.encodeClass())
+                                }
+                                .padding(horizontal = 16.dp, vertical = 4.dp),
+                            text = breed.getDisplayString(),
+                            style = typography.bodyLarge
+                        )
+                    }
                 }
             }
         }
     }
+}
+
+@Composable fun SearchField(
+    query: String,
+    onChanged: (String) -> Unit
+) {
+    TextField(
+        modifier = Modifier.fillMaxWidth(),
+        singleLine = true,
+        value = query,
+        onValueChange = onChanged,
+        placeholder = {
+            Text(
+                text = stringResource(R.string.search),
+                style = typography.titleSmall
+            )
+        },
+        trailingIcon = {
+            if(query.isNotEmpty()) {
+                IconButton(onClick = { onChanged("") }) {
+                    Icon(
+                        imageVector = Icons.Default.Close,
+                        contentDescription = stringResource(R.string.clear)
+                    )
+                }
+            }
+        }
+    )
 }
